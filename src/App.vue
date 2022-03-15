58e875fe80bf6e49851cab13bd2afa2af1065e06
leohai <448456376@qq.com>
2022-03-15 15:58:19 +0800

    feat: qiankuan主应用修改

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/vue">vue</router-link> |
+    <router-link to="/angular">angular</router-link>
+    <div id="vue-container" v-show="$route.path.startsWith('/vue')"></div>
+    <div
+      id="angular-container"
+      v-show="$route.path.startsWith('/angular')"
+    ></div>
   </div>
-  <router-view />
 </template>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
 import store from './store';
+import router from './router';
+import { registerMicroApps, start } from 'qiankun';
 
+registerMicroApps([
+  {
+    name: 'vueApp', // app name registered
+    entry: '//localhost:8080',
+    container: '#vue-container',
+    activeRule: '#/vue',
+  },
+  {
+    name: 'angularApp',
+    entry: '//localhost:5500/subApp/angularjs/www/index.html',
+    container: '#angular-container',
+    activeRule: '#/angular',
+  },
+]);
+
+start({ sandbox: false });
 createApp(App).use(store).use(router).mount('#app');
